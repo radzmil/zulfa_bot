cbffff3121b0c2f918567d2d6ddcf458ec5f8317
radzmil <radzmil@gmail.com>
2026-08-19 12:56:13 +0800

    Fix port untuk render

--- zulfa_bot.py
+++ zulfa_bot.py
@@ -182,5 +182,4 @@ def whatsapp_webhook():
         return jsonify({"status": "error"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
