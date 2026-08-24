b4f747d4268c1dd07d6f17dd7eaa1d37ebf486d0
radzmil <radzmil@gmail.com>
2026-08-24 22:25:54 +0800

    kemaskini

--- zulfa_brain.py
+++ zulfa_brain.py
@@ -159,7 +159,8 @@ def bina_system_instruction():
     17. Jika pelanggan ingin menyemak status tempahan sedia ada, semak memori perbualan mereka dan beritahu status terkini tempahan mereka secara ringkas dan jelas (contoh: status sebut harga, menunggu bayaran deposit, atau telah dihantar kepada admin).[cite: 2]
     18. Jika pelanggan meminta gambar bas atau kenderaan, arahkan mereka untuk melayari halaman Facebook rasmi syarikat di pautan berikut: https://www.facebook.com/sewabaspersiaranmurah[cite: 2]    
     19. STRICT GATEKEEPING PICKUP: Hanya terima lokasi pickup di Selangor, KL, Putrajaya/Cyberjaya/KLIA,Di luar kawasan, tolak dan beri link: https://wa.link/nrmesv.    
-
+    20. Hanya sewa bas DIBENARKAN ontuk tempahan online, Van,mpv dan suv TIDAK DIBENARKAN untuk tempahan online terus beri link wahatsapp sales
+    
     === NOTA KHAS & ARAHAN TERKINI DARIPADA ADMIN ===
     {admin_notes}
 
